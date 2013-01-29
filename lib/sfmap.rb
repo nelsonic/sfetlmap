c503762dc19138b25444c3f07f271200eabab596
nelsonic <nodecoder@gmail.com>
2013-01-29 17:50:32 -0600

    Tidied up a bit - still work to do... but all tests passing

--- lib/sfmap.rb
+++ lib/sfmap.rb
@@ -3,50 +3,53 @@ require 'google_drive'
 require 'restforce'
 
 class Sfmap
-  @@conf =  EtlMapping::Config::config # instanciates config method
-  @@gconf = @@conf['google']  # pull out google creds
-  @@origin_env = 'emea_prod' # default "origin" sf org
-  @@target_env = 'sbus'
-  @@key = 'copy'      # default GDrive Spreadsheet key
-  @@origin_org = "EMEA"
-  @@target_org = "US"
-  @@origin_client
-  @@target_org_objects = []
-  @@gdrive_session
-  @@spreadsheet
-  @origin_sheet
-  @@sheets = []
-  @@excluded_objects_patterns = [
-    "AccountHistory", "AccountPartner", "AccountShare", "AccountTag","AccountTeamMember",
-    "AccountTerritory", "ActivityHistory","AdditionalNumber", "AggregateResult", "Approval",
-    "AssetTag","AssignmentRule", "Attachment",
-    "Apex", "APXT_", "BrandTemplate", "CaseTeam",
-    "CampaignMemberStatus","CampaignShare","CampaignTag",
-    "FISH_SYSTEM", "echosign", "ff", "Field_Trip",
-    "IDE", "iformDemo", "natterbox",
-    "__History","__Share", "__Tag",
-    "Period","ProcessInstance",
-    "SFGA", "shc__",
-    "TIMBASURVEYS", "TRIMDA", "tquilamobile", "Vote", "Zendesk"
-  ]
+  attr_accessor :conf
 
+  def key
+    @key ||= 'copy'
+  end
+
+  def conf
+    @conf ||=  EtlMapping::Config::config # instanciates config method
+  end
 
   def gconf
-    @@gconf
+    @conf  ||= conf
+    @gconf ||= @conf['google']  # pull out google creds
   end
 
   def origin_org
-    @@origin_org
+    @origin_org ||= "EMEA"
+  end
+
+  def origin_env
+    @origin_env ||= 'emea_prod' # default "origin" sf org
   end
 
   def target_org
-    @@target_org
+    @target_org
+  end
+
+  def excluded_objects_patterns # this should be in a User-Editable SpreadSheet!!
+    @excluded_objects_patterns ||= [
+    "AccountHistory", "AccountPartner", "AccountShare", "AccountTag",
+    "AccountTeamMember", "AccountTerritory", "ActivityHistory","AdditionalNumber",
+    "AggregateResult", "Approval", "AssetTag","AssignmentRule", "Attachment",
+    "Apex", "APXT_", "BrandTemplate", "CaseTeam", "CampaignMemberStatus",
+    "CampaignShare","CampaignTag", "FISH_SYSTEM", "echosign", "ff", "Field_Trip",
+    "IDE", "iformDemo", "natterbox", "__History","__Share", "__Tag","Period",
+    "ProcessInstance", "SFGA", "shc__","TIMBASURVEYS", "TRIMDA", "tquilamobile",
+    "Vote", "Zendesk"
+  ]
   end
 
   def restforce_connect(env)
-    env ||= @@origin_env
-    sfconf = @@conf[env]["salesforce"]   # pull out creds for the env instance
-    @@client = Restforce.new :username => sfconf["user"],
+    env ||= @origin_env
+    @conf ||= conf
+    # puts "Env: #{env}"
+    sfconf = @conf[env]["salesforce"]   # pull out creds for the env instance
+    # puts "SF Conf: #{sfconf}"
+    @client = Restforce.new :username => sfconf["user"],
       :password       => sfconf["password"],
       :security_token => sfconf["token"],
       :client_id      => sfconf["client_id"],
@@ -54,14 +57,16 @@ class Sfmap
   end
 
   def origin_client
-    @@origin_client = restforce_connect(@@origin_env)
+    @origin_client ||= restforce_connect(@origin_env)
   end
 
-  def target_client
-    @@target_client = restforce_connect(@@target_env)
-  end
+  # def target_client
+    # @@target_client = restforce_connect(@@target_env)
+  # end
 
   def list_salesforce_objects(client)
+      # @excluded_objects_patterns ||= excluded_objects_patterns
+      client ||= @client
       result = client.describe
       sf_objects = []
       result.each do |item|
@@ -71,79 +76,69 @@ class Sfmap
 
       # exclude the unwanted objects/patterns using Regexp @@excluded_objects_patterns
       # http://stackoverflow.com/questions/9731649/match-a-string-against-multiple-paterns
-      re = Regexp.union(@@excluded_objects_patterns)
-      sf_objects.select{ |obj| !obj.match(re).nil? }.map{ |o| sf_objects.delete(o)}
+      # re = Regexp.union(excluded_objects_patterns)
+      # sf_objects.select{ |obj| !obj.match(re).nil? }.map{ |o| sf_objects.delete(o)}
 
       sf_objects
   end
 
   def origin_org_objects
-    @origin_org_objects ||= list_salesforce_objects(@@origin_client)
+    @origin_org_objects ||= list_salesforce_objects(@origin_client)
   end
 
 
   def gdrive_session
-    @@gdrive_session = GoogleDrive.login(gconf["user"], gconf["password"])
+    @gdrive_session ||= GoogleDrive.login(gconf["user"], gconf["password"])
   end
 
 
   def open_google_spreadsheet(key)
-    @@gdrive_session ||= gdrive_session
-    @@key = key ||= @@key
+    @gdrive_session ||= gdrive_session
     sheet_key = gconf[key]
-    @@spreadsheet = @@gdrive_session.spreadsheet_by_key(sheet_key)
+    @spreadsheet ||= @gdrive_session.spreadsheet_by_key(sheet_key)
   end
 
   def spreadsheet
-    @@spreadsheet = open_google_spreadsheet(@@key)
+    @key ||= key
+    @spreadsheet ||= open_google_spreadsheet(@key)
   end
 
 
   def sheets
-    @@spreadsheet ||= open_google_spreadsheet(@@key)
-    @@spreadsheet.worksheets.each do |sheet|
-      @@sheets << sheet.title
+    @sheets ||= []
+    if @sheets.size == 0
+      @key ||= key
+      # puts "***** Key: #{@key} *****"
+      @spreadsheet ||= open_google_spreadsheet(@key)
+      @spreadsheet.worksheets.each do |sheet|
+        @sheets << sheet.title
+      end
     end
-    @@sheets
+    @sheets
   end
 
   def find_worksheet_by title
-    spreadsheet.worksheets.find {|ws| ws.title == title}
+    @spreadsheet ||= spreadsheet
+    @spreadsheet.worksheets.find {|ws| ws.title == title}
   end
 
   def origin_sheet sheet_name
     @origin_sheet ||= find_worksheet_by sheet_name
   end
 
-  def list_sf_obejcts_in_sheet(sheet_name,column)
-    @origin_sheet ||= origin_sheet sheet_name
+  def list_sf_obejcts_in_sheet(ws,column)
     origin_objects_in_sheet = []
     i = 1
-    first_empty_cell =  find_first_empty_cell_in_col(sheet_name,column)
-    until i > first_empty_cell
+    # first_empty_cell =  find_first_empty_cell_in_col(sheet_name,column)
+    until i > ws.max_rows
       i = i + 1
-      origin_objects_in_sheet <<  object = @origin_sheet[i, column]
+      origin_objects_in_sheet <<  ws[i, column]
     end
     origin_objects_in_sheet.reject! { |c| c.empty? }
-    origin_objects_in_sheet.delete("#{@@origin_org} org")
+    origin_objects_in_sheet.delete("#{@origin_org} org")
     origin_objects_in_sheet.delete("Object")
     # puts i, origin_objects_in_sheet.length
     origin_objects_in_sheet
-  end
-
-  def find_first_empty_cell_in_col(sheet_name,column)
-    @origin_sheet ||= origin_sheet sheet_name
-    i = 4 # data usually only starts on 4th row
-    empty_cell = 0
-    until empty_cell > 0
-      cell = @origin_sheet[i, column]
-      if cell.to_s.strip.length == 0
-        empty_cell = i
-      end
-      i = i + 1
-    end
-    # puts "First Empty Cell = #{empty_cell}"
-    empty_cell
   end
 
   def list_sf_objects_not_in_origin_sheet(origin_objects, objects_in_sheet)
@@ -158,14 +153,79 @@ class Sfmap
     origin_objects_not_in_sheet
   end
 
-   def insert_origin_objects_into_sheet(sheet_name, column, empty_cell, objects_to_add)
-    @origin_sheet ||= origin_sheet sheet_name
+   def insert_origin_objects_into_sheet(ws, column, objects_to_add)
+    empty_cell = find_first_empty_cell_in_col(ws,column)
     objects_to_add.each { |obj|
       # puts "Cell: #{empty_cell} | Obj: #{obj}"
-      @origin_sheet[empty_cell, column] = obj.to_s
+      ws[empty_cell, column] = obj.to_s
       empty_cell = empty_cell + 1
     }
-    @origin_sheet.save()
+    ws.save()
    end
+
+   # Updating the Individual Sheets is a bit more Complex than it needed to be.
+   # Some sheets have the Field name colum in the 2nd column others in the 3rd/4th
+   # so our script needs to "learn" which column is for which data
+   # field_name_col, field_type_col, field_default_col
+   # the function will take the sheet and a hash of fields as parameters
+   # we recycle find_first_empty_cell method
+
+   def find_column_number_by_name_in_sheet(ws,col_name)
+      across = 1
+      down   = 1
+      until ws[down,across] == col_name || down == ws.num_rows
+        if(across == 10 ) # ws.num_cols) # max columns in sheet
+          across = 1         # reset column index to 1
+          down   = down + 1  # increment row count
+        else
+          across = across + 1 # check next column
+        end
+      end
+      if down == ws.num_rows
+      end
+      across # i.e. return the column index
+   end
+
+   def list_obejct_fields_in_sheet(ws,column)
+    object_fields = []
+    i = 1
+    empty_cell = find_first_empty_cell_in_col(ws,column)
+    until i > ws.num_rows
+      i = i + 1
+      object_fields << ws[i, column] # field_name
+    end
+    object_fields.reject! { |c| c.empty? }
+    # puts i,    origin_objects_in_sheetlength
+    object_fields
+   end
+
+   def find_first_empty_cell_in_col(ws,column)
+    i = 4 # data usually only starts on 4th row
+    empty_cell = 0
+    until empty_cell > 0 # && (empty_cell + 2).to_s.strip.length == 0
+      cell = ws[i, column]
+      if cell.to_s.strip.length == 0 and ws[i+1, column].to_s.strip.length == 0 and ws[i+2, column].to_s.strip.length == 0
+        empty_cell = i
+      end
+      i = i + 1
+    end
+    # puts "First Empty Cell = #{empty_cell}"
+    empty_cell
+  end
+
+  def sheets_to_update
+    @sheets_to_update = []
+    @origin_org_objects ||= origin_org_objects
+    @sheets ||= sheets
+    @sheets.each { |sheet|
+      if @origin_org_objects.include? sheet
+        @sheets_to_update << sheet
+      end
+    }
+    @sheets_to_update
+  end
+
+
+
 
 end
--- spec/sfmap_spec.rb
+++ spec/sfmap_spec.rb
@@ -5,16 +5,20 @@ require 'config'
 describe Sfmap do
 
   before(:all) do
-    subject.origin_client
-    subject.gdrive_session
-    # subject.origin_org_objects
+      @origin_client = subject.restforce_connect(subject.origin_env)
+      @origin_objects =  subject.origin_org_objects
+      subject.gdrive_session
+      @origin_ws = subject.find_worksheet_by "#{subject.origin_org} Objects"
+      @origin_objects_in_sheet = subject.list_sf_obejcts_in_sheet(@origin_ws,1)
+      @objects_not_in_sheet = subject.list_sf_objects_not_in_origin_sheet(@origin_objects, @origin_objects_in_sheet)
+      @account_ws = subject.find_worksheet_by 'Account'
+      @sheets = subject.sheets
   end
 
   it 'Retrieves List of "Origin" Org SF Object' do
       # SF has Lots of SObjects! See:
       # http://www.salesforce.com/us/developer/docs/officetoolkit/Content/sforce_api_objects_list.htm
-      origin_objects = subject.origin_org_objects
-      expect(origin_objects.length).to be > 40
+      expect(@origin_objects.length).to be > 40
   end
 
   it 'Connects to Google Drive Checks Spreadsheet exists' do
@@ -22,33 +26,80 @@ describe Sfmap do
   end
 
   it 'Checks if the All SF Objects are in "Origin" Object Sheet' do
-      origin_objects = subject.origin_org_objects
-      sheet =  "#{subject.origin_org} Objects"
-      col = 1
-      empty_cell = subject.find_first_empty_cell_in_col(sheet,col)
-      objects_in_sheet = subject.list_sf_obejcts_in_sheet(sheet,col)
-      objects_not_in_sheet = subject.list_sf_objects_not_in_origin_sheet(origin_objects, objects_in_sheet)
+      puts "Objects not in Sheet: #{@objects_not_in_sheet.length}"
+      puts "Number of Origin Objects in SF: #{@origin_objects.length}"
+      puts "Number of Objects in Sheet: #{@origin_objects_in_sheet.length}"
 
-      puts "Objects not in Sheet: #{objects_not_in_sheet.length}"
-      puts "Number of Origin Objects in SF: #{origin_objects.length}"
-      puts "Number of Objects in Sheet: #{objects_in_sheet.length}"
-
-      if objects_not_in_sheet.size > 0
+      if @objects_not_in_sheet.size > 0
         # Try to update the list of objects in the Origin Sheet:
-        subject.insert_origin_objects_into_sheet(sheet, col, empty_cell, objects_not_in_sheet)
-        objects_in_sheet = subject.list_sf_obejcts_in_sheet(sheet,col)
-        objects_not_in_sheet = subject.list_sf_objects_not_in_origin_sheet(origin_objects, objects_in_sheet)
+        subject.insert_origin_objects_into_sheet(@origin_ws, 1, @objects_not_in_sheet)
+        @origin_objects_in_sheet = subject.list_sf_obejcts_in_sheet(@origin_ws,1)
+        @objects_not_in_sheet =
+          subject.list_sf_objects_not_in_origin_sheet(@origin_objects, @origin_objects_in_sheet)
       end
-      expect(objects_not_in_sheet.size) .to eq(0)
+      expect(@objects_not_in_sheet.size).to eq(0)
   end
 
-  it 'Updates each sheet in the Spreadsheet with any new fields' do
-      sheets = subject.sheets
-      # puts sheets
-      origin_objects = subject.origin_org_objects
-      # puts origin_objects
-      # if the sheet name/title is an SF object try a "describe" on it.
+  it 'Finds the Column where the Field Name is located in a Sheet' do
+      col_num = subject.find_column_number_by_name_in_sheet(@account_ws,'Field')
+      puts "Account 'Field' column is in #{col_num}"
+      expect(col_num).to eq(3)
+      # col_num = subject.find_column_number_by_name_in_sheet('Type',sheet_name)
+      # expect(col_num).to eq(6)
+      # col_num = subject.find_column_number_by_name_in_sheet('Default Value',sheet_name)
+      # expect(col_num).to eq(7)
+  end
 
+  it 'Find Fields in Account Sheet and Confirm Phone exists' do
+      col_num = subject.find_column_number_by_name_in_sheet(@account_ws,'Field')
+      expect(col_num).to eq(3)
+      account_fields = subject.list_obejct_fields_in_sheet(@account_ws,col_num)
+      # puts account_fields
+      expect(account_fields.include? 'Phone').to eq(true)
+
+  end
+
+
+
+  it 'Loop through the list of sheets and describe the SF Object' do
+
+      ws = subject.find_worksheet_by 'US to EMEA Mappings'
+      column = 5
+      @sheets_to_update = subject.list_obejct_fields_in_sheet(ws,column)
+      puts @sheets_to_update
+
+      @sheets_to_update.each do |obj|
+        puts "*** Updating #{obj} ***"
+        ws = subject.find_worksheet_by obj
+        field_col = col_num = subject.find_column_number_by_name_in_sheet(ws,'Field')
+        object_fields_in_sheet = subject.list_obejct_fields_in_sheet(ws,field_col)
+        obj_fields = {}
+        result = @origin_client.describe(obj)
+        result.fields.each do |item|
+          if !object_fields_in_sheet.include? item.name
+            obj_fields[item.name] = {"name"=> item.name,
+                                    "type" => item.type,
+                                    "default" => item.defaultValue }
+          end
+        end
+
+        type_col    = subject.find_column_number_by_name_in_sheet(ws,'Type')
+        default_col = subject.find_column_number_by_name_in_sheet(ws,'Default Value')
+        empty_cell  = subject.find_first_empty_cell_in_col(ws,field_col)
+        obj_fields.each do |key,value|
+          puts " #{empty_cell} v: #{value['name']} t: #{value['type']} d: #{value['default']}"
+          ws[empty_cell, field_col]   = value["name"]
+          ws[empty_cell, type_col]    = value["type"]
+          ws[empty_cell, default_col] = value["default"]
+          empty_cell += 1
+        end
+        ws.save()
+      end
+  end
+
+  it ' ' do
+    # result = @origin_client.describe
+    # puts result
 
   end
 
